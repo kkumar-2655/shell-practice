f2791f974f0baa19b633b62e56ee45bf67250d2c
Kiran <kirann.bobby@gmail.com>
2025-05-15 16:04:53 +0530

    hello world

---
+++ 02-conversation.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=Trump
+PERSON2=Modi
+
+
+echo "$PERSON1: Hey $PERSON2 How was the war going"
+echo "$PERSON2: Going great and unstoppable"
+echo "$PERSON1:: Please stop the war as pakistan is scrumbling"
+echo "$PERSON2: If they stop , we will stop!"
