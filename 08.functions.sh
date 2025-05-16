47cc14eaf38dabadb6afdb72a69a7fc73fbe7f20
Kiran <kirann.bobby@gmail.com>
2025-05-16 16:26:59 +0530

    jenkins installation

--- 08.functions.sh
+++ 08.functions.sh
@@ -1,93 +1,91 @@
 #!/bin/bash
 
+# Check if user is root
 USERID=$(id -u)
 if [ "$USERID" -ne 0 ]; then
   echo "Please run as root."
   exit 1
 fi
 
+# Update system
 dnf update -y
-
-if [ $?==0 ]; then
+if [ $? -eq 0 ]; then
   echo "Update successful."
 else
   echo "Update failed."
   exit 1
 fi
 
-
-
+# Upgrade system
 dnf upgrade -y
-if [ $?==0 ]; then
+if [ $? -eq 0 ]; then
   echo "Upgrade successful."
 else
   echo "Upgrade failed."
   exit 1
 fi
 
+# Install Java
 dnf install -y java-17-openjdk
-
-if [ $?==0 ]; then
+if [ $? -eq 0 ]; then
   echo "Java installation successful."
 else
   echo "Java installation failed."
   exit 1
 fi
 
-dnf install wget -y
-if [ $?==0 ]; then
+# Install wget
+dnf install -y wget
+if [ $? -eq 0 ]; then
   echo "Wget installation successful."
 else
   echo "Wget installation failed."
   exit 1
 fi
 
-
-
-wget -O /etc/yum.repos.d/jenkins.repo \
-    https://pkg.jenkins.io/redhat-stable/jenkins.repo
-
-if [ $?==0 ]; then
+# Add Jenkins repo
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+if [ $? -eq 0 ]; then
   echo "Jenkins repo added successfully."
 else
   echo "Failed to add Jenkins repo."
   exit 1
 fi
 
+# Import Jenkins key
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-if [ $?==0 ]; then
+if [ $? -eq 0 ]; then
   echo "Jenkins key imported successfully."
 else
   echo "Failed to import Jenkins key."
   exit 1
 fi
 
-
-
+# Install Jenkins
 dnf install -y jenkins
-
-
-if [ $?==0 ]; then
+if [ $? -eq 0 ]; then
   echo "Jenkins installation successful."
 else
   echo "Jenkins installation failed."
   exit 1
 fi
 
-systemctl enable Jenkins
-
-if [ $?==0 ]; then
+# Enable Jenkins service
+systemctl enable jenkins
+if [ $? -eq 0 ]; then
   echo "Jenkins service enabled successfully."
 else
   echo "Failed to enable Jenkins service."
   exit 1
 fi
 
-systemctl start Jenkins
-if [ $?==0 ]; then
+# Start Jenkins service
+systemctl start jenkins
+if [ $? -eq 0 ]; then
   echo "Jenkins service started successfully."
 else
   echo "Failed to start Jenkins service."
   exit 1
 fi
+
 
