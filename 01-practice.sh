b51906fff5cf91fc8e21b96ef37d7ea2fc57ad3d
Kiran <kirann.bobby@gmail.com>
2025-05-17 11:41:26 +0530

    cpu usage

--- 01-practice.sh
+++ 01-practice.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-CPU=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
-if (( $(echo "$CPU > 80" | bc -l) )); then
+CPU=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.0f\n", 100 - $8}')
+if [ "$CPU" -gt 80 ]; then
   echo "High CPU usage: $CPU%"
 fi
