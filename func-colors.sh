d88286a555b8a3e58f27ee5bdf2220a4b4cc2e5a
Kiran <kirann.bobby@gmail.com>
2025-05-20 12:33:25 +0530

    functions colors

--- func-colors.sh
+++ func-colors.sh
@@ -11,43 +11,43 @@ N="\e[0m"
 
 
 if [ "$USERID" -ne 0 ]; then
-  echo "$R Please run as root. $N"
+  echo "$R Please run as root $N"
   exit 1
 fi
 
 # Update system
 dnf update -y
 if [ $? -eq 0 ]; then
-  echo "$G Update successful. $N"
+  echo "$G Update successful $N"
 else
-  echo "$R Update failed. $N"
+  echo "$R Update failed $N"
   exit 1
 fi
 
 # Upgrade system
 dnf upgrade -y
 if [ $? -eq 0 ]; then
-  echo "$G Upgrade successful. $N"
+  echo "$G Upgrade successful $N"
 else
-  echo "$R Upgrade failed. $N"
+  echo "$R Upgrade failed $N"
   exit 1
 fi
 
 # Install Java
 dnf install -y java-17-openjdk
 if [ $? -eq 0 ]; then
-  echo "$G Java installation successful. $N"
+  echo "$G Java installation successful $N"
 else
-  echo "$R Java installation failed. $N"
+  echo "$R Java installation failed $N"
   exit 1
 fi
 
 # Install wget
 dnf install -y wget
 if [ $? -eq 0 ]; then
-  echo "$G Wget installation successful. $N"
+  echo "$G Wget installation successful $N"
 else
-  echo "$R Wget installation failed. $N"
+  echo "$R Wget installation failed $N"
   exit 1
 fi
 
@@ -55,45 +55,45 @@ fi
 curl -o /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 
 if [ $? -eq 0 ]; then
-  echo "$G Jenkins repo added successfully. $N"
+  echo "$G Jenkins repo added successfully $N"
 else
-  echo "$R Failed to add Jenkins repo. $N"
+  echo "$R Failed to add Jenkins repo $N"
   exit 1
 fi
 
 # Import Jenkins key
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 if [ $? -eq 0 ]; then
-  echo "$G Jenkins key imported successfully. $N"
+  echo "$G Jenkins key imported successfully $N"
 else
-  echo "$R Failed to import Jenkins key. $N"
+  echo "$R Failed to import Jenkins key $N"
   exit 1
 fi
 
 # Install Jenkins
 dnf install -y jenkins
 if [ $? -eq 0 ]; then
-  echo "$G Jenkins installation successful. $N"
+  echo "$G Jenkins installation successful $N"
 else
-  echo "$R Jenkins installation failed. $N"
+  echo "$R Jenkins installation failed $N"
   exit 1
 fi
 
 # Enable Jenkins service
 systemctl enable jenkins
 if [ $? -eq 0 ]; then
-  echo "$G Jenkins service enabled successfully. $N"
+  echo "$G Jenkins service enabled successfully $N"
 else
-  echo "$R Failed to enable Jenkins service. $N"
+  echo "$R Failed to enable Jenkins service $N"
   exit 1
 fi
 
 # Start Jenkins service
 systemctl start jenkins
 if [ $? -eq 0 ]; then
-  echo "$G Jenkins service started successfully. $N"
+  echo "$G Jenkins service started successfully $N"
 else
-  echo "$R Failed to start Jenkins service. $N"
+  echo "$R Failed to start Jenkins service $N"
   exit 1
 fi
 
